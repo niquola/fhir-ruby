bdbc7cebcf3adf38e0107684c9dbabb1c9be453e
nicola <niquola@gmail.com>
2013-07-15 21:00:09 +0400

    fix spec

--- spec/meta/modles_builder_spec.rb
+++ spec/meta/modles_builder_spec.rb
@@ -6,10 +6,17 @@ file_name = File.join(File.dirname(__FILE__), '..', '..', 'fhir-base.xsd')
 Fhir::Meta::Datatype.load(file_name)
 
 describe "Fhir::Meta::ModelsBuilder" do
-  let(:builder) { Fhir::Meta::ModelsBuilder.new }
+  before(:all) do
+    builder = Fhir::Meta::ModelsBuilder.new
+    builder.build
+    @models = builder.models
+  end
+
+  def models
+    @models
+  end
 
   example do
-    models = builder.build
     medication_statment = models.find do |m|
       m.full_name == "MedicationStatement"
     end
@@ -27,19 +34,19 @@ describe "Fhir::Meta::ModelsBuilder" do
   end
 
   example do
-    identifier = builder.build.find do |m|
+    identifier = models.find do |m|
       m.full_name == "MedicationStatementIdentifier"
     end
     identifier.attributes.size.should == 4
 
-    coding = builder.build.find do |m|
+    coding = models.find do |m|
       m.full_name == "MedicationStatementDosageSiteCoding"
     end
     coding.should_not be_nil
   end
 
   it "should fill refs" do
-    statement = builder.build.find do |m|
+    statement = models.find do |m|
       m.full_name == "MedicationStatement"
     end
 
@@ -48,34 +55,35 @@ describe "Fhir::Meta::ModelsBuilder" do
   end
 
   it "should fill refs" do
-    medication = builder.build.find do |m|
+    medication = models.find do |m|
       m.full_name == "MedicationStatementMedication"
     end
     medication.attributes.should_not be_empty
 
     #p medication
 
-    medication_coding = builder.build.find do |m|
+    medication_coding = models.find do |m|
       m.full_name == "MedicationStatementMedicationCodeCoding"
     end
     medication_coding.should_not be_nil
   end
 
   example do
-    models = builder.build
     models.sort_by(&:full_name).each do |m|
       puts '-'*20
       puts m.full_name
       puts "parent: #{m.parent && m.parent.full_name}"
-        puts "resource_refs:"
+
+      puts "resource_refs:"
       m.resource_refs.each do |r|
         puts "* #{r.name} #{r.resources}"
       end
-      puts "attributes"
+
       puts "attributes"
       m.attributes.each do |a|
         puts "* #{a.name} #{a.type}"
       end
+
       puts "associations"
       m.associations.each do |assoc|
         puts "* #{assoc.inspect}"
